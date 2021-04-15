6aeea17fb91a8e5b945399248ff2b4d2df5139b9
Matt <ok7140mj@go.minnstate.edu>
2021-04-15 15:08:15 -0500

    Changed Message with date

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', function(req, res, next){
-    res.json( { 'message': 'Hello! I am working!'} )
+    res.json( { 'message': 'Hello! I am working! Fixed 4/15/2021'} )
 })
 
 module.exports = router
